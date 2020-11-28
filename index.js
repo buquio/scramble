2038278a8d236183e3e3d7615b8907cf77e7dc68
buquio <kehinde.bukola14@gmail.com>
2020-11-28 14:20:44 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+//Final Challenge
+
+//Challenge: Using all your knowledge from this course, add 5 words to 
+//your scrabble board using only one for loop. 
+
+//As this challenge is the last in this course you will be expected to rely on
+//all the material you have learnt so far.
+
+//If you do get stuck please do reach out to the Scrimba community on Discord or
+//or feel free to google.
+
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+console.log(squares)
+
+const layout = [
+    's','c','r','a','b','b','l','e','','','','','',
+    '','o','','','','','','','','','','','',
+    '','w','i','n','n','e','r','','','','','','',
+    '','','','','','','i','','','','','','',
+    '','','','','','','n','','','','','','',
+    '','','','','','','g','o','a','t','','','',
+    '','','','','','','','','','','','','',
+    '','','','','','','','','','','','','',
+    '','','','','','','','','','','','','',
+    '','','','','','','','','','','','','',
+    '','','','','','','','','','','','','',
+    '','','','','','','','','','','','','',
+    '','','','','','','','','','','','','',
+]
+
+// declare a variable to store the letter
+// innerHTML
+// style the div with a class
+
+for (let i=0; i < squares.length; i++) {
+    let letter = layout[i]
+    letter = squares[i].innerHTML 
+    if (letter !== '') squares[i].classList.add('tile')
+}
+
+
+
